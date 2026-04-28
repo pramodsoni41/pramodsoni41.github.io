fe64bac4cf880c2a45aeb29c74376825d345b4ca
Pramod Soni <pramodsoni41@gmail.com>
2026-04-28 18:55:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzBAiA_aF560uVgWFCzkf3n9_9t3_LZ3UkomHlLcmxI-M7GTJEry_DWLrevuam8qxc/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby9EaN5qEmb26795b-um7USzpOLCJLms6ti9Jaoc6n9Dbce8tKrUt_psaDZLtLdM2E/exec"
 };
