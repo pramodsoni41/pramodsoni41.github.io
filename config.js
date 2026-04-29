e37d3876dcee8321e7e7204fa1400c4b5a6b6d98
Pramod Soni <pramodsoni41@gmail.com>
2026-04-29 10:35:28 +0530

    ss

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw_3vAfoIvH-T9V5iNqC3oor-ft4dyFKKursJmKlv7TJ3yDn7zE2gSW8PzZmWQpQdw/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxwuu_zxQPFzl1xJb6_jGjzjD6_2Fs04bLLLNnEGs9XaaIoKW8fqX-RKv0CBj7LTwI/exec"
 };
